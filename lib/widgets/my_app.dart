76e2ec58454346b08586b27e48bb8955cc6dab2e
abdlerahman ayman <abdelrahmanayman559@gmail.com>
2025-09-17 23:29:43 +0300

    feat: Implement splash screen customization for Android and iOS
    - Added new splash screen images for different resolutions and dark mode support.
    - Updated launch_background.xml to use new splash images.
    - Created styles for both light and dark themes in values-night and values-v31.
    - Configured flutter_native_splash.yaml for splash screen settings.
    - Modified iOS LaunchScreen.storyboard to include new background images.
    - Updated Info.plist to ensure proper splash screen display.
    - Refactored main.dart to use MyApp widget and set up routing.
    - Created HomeScreen widget for the main application interface.
    - Updated web/index.html for splash screen handling and added responsive styles.
    - Added splash images for web in light and dark modes.

---
+++ lib/widgets/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:alfurqan/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
